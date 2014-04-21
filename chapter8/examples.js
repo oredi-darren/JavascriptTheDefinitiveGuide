4120ae749042926bf75d6ce5f30cfbd528680f0f
Darren Seet Serkai <darren@seet.me>
2014-04-21 17:46:44 +0900

    Complete basic functional programming

--- chapter8/examples.js
+++ chapter8/examples.js
@@ -38,3 +38,123 @@ function operate2(operator, operand1, operand2) {
 var j = operate2("add", "hello", operate2("add", " ", "world"))
 // Using the predefined Math.pow() function:
 var k = operate2("pow", 10, 2);
+
+var scope = "global scope";
+function checkscope() {
+    var scope = "local scope";
+    function f()  { return scope; }
+    return f();
+}
+
+checkscope();
+
+function checkclosurescope() {
+    var scope = "local scope";
+    function f()  { return scope; }
+    return f;
+}
+checkclosurescope()();
+
+var unqiueInteger = (function() {   // Define and invoke
+    var counter = 0;    // private state of function below
+    return function() { return counter++; }
+}());
+
+function counter() {
+    var n = 0;
+    return {
+        count: function() { return n++; },
+        reset: function() { n = 0; }
+    }
+}
+
+var c = counter(), d = counter();
+
+function counter(n) {   // Function argument n is the private variable
+    return {
+        // Property getter method returns and increments private counter var.
+        get count() { return n++; },
+        // Property setter method doesn't allow the value of n to decrease
+        set count(m) {
+            if(m >= n) n = m;
+            else throw Error("count can only be set to a larger value")
+        }
+    }
+}
+
+// The following code demonstrates the addPrivateProperty() method.
+var o = {}; // Here is an empty object
+
+// add property accessor methods getName and setName
+// Ensure that only string values are allowed
+addPrivateProperty(o, "Name", function(x) { return typeof x == "string"; });
+
+// closure sharing variable
+// This function returns a function that always returns v
+function constfunc(v) { return function() { return v; }; }
+
+// Create an array of constant functions
+var funcs = [];
+for (var i = 0; i < 10; i++) funcs[i] = constfunc(i);
+
+// The function at array element 5 returns the value 5
+funcs[5]();
+
+// This function returns a function that always returns v
+function constsharedfuncs(v) { 
+    var funcs = [];
+    for (var i = 0; i < 10; i++) 
+        funcs[i] = function() { return i; };    // i is a closure variable so it is set to 10 after the last execution
+    return funcs; 
+}
+
+var funcs = constsharedfuncs();
+funcs[5]();
+
+
+// length property of function returns the number of variables in a declared in a function
+// This function uses arguments.callee, so it won't work in strict mode
+function check(args) {
+    var actual = args.length;           // The actual number of arguments
+    var expected = args.callee.length;  // The expected number of arguments
+    if(actual !== expected)             // Throw an exception if they differ
+        throw Error("Expected " + expected + " args; got " + actual);
+}
+
+function f(x, y, z) {
+    check(arguments);   // Check that the actual # of args matches expected #.
+    return x + y + z;   // Now do the rest of the function normally
+}
+
+function f(y) { return this.x + y; }    // This function needs to be bound
+var o = { x: 1 };                       // An object we'll bind to
+var g = f.bind(o);                      // Calling g(x) invokes o.f(x)
+g(2);
+
+
+
+// Using partial application and binding to do currying
+var sum = function(x,y) { return x + y };   // Return the sum of 2 args
+// Create a new function like sum, but with the this value bound to null
+// and the 1st argument bound to 1. This new function expects just one arg.
+var succ = sum.bind(null, 1);
+succ(2);
+
+function f(y,z) { return this.x + y + z };  // another functino that adds
+var g = f.bind({x:1}, 2);                   // Bind this and y
+g(3);
+
+// A Closure is a combination of a function object and a scope (a set of variable bindings) in t\which the function's variables are resolved
+
+
+// Functional programming
+// First, define two simple functions
+var sum = function(x,y) { return x + y; };
+var square = function(x) { return x * x; };
+
+// Then use those functions with Array methods to compute mean and stddev
+var data = [1,1,3,5,5];
+var mean = data.reduce(sum)/data.length;
+
+var deviations = data.map(function(x) { return x-mean; });
+var stddev = Math.sqrt(deviations.map(square).reduce(sum)/(data.length-1));
--- chapter8/utility.js
+++ chapter8/utility.js
@@ -181,3 +181,127 @@ function isArrayLike(o) {
     else
         return false;                           // Otherwise it is not
 }
+
+// This function adds property accessor methods for a property with
+// the specified name to the object o. The methods are named get<name>
+// and set<name>. If a predicate function is supplied, the setter
+// method uses it to test its argument for validity before storing it.
+// If the predicate returns false, the setter method throws an exception.
+//
+// The unusual thing about this function is that the property value
+// that is manipulated by the getter and setter methods is not stored in
+// the object o. Instead, the value is stored only in a local variable
+// in this function. The getter and setter methods are also defined
+// locally to this function and therefore have access to this local variable.
+// This means that the value is private to the two accessor methods, and it
+// cannot be set of modified except through the setter method.
+function addPrivateProperty(o, name, predicate) {
+    var value;  // This is the property value
+
+    // The getter method simply returns the value.
+    o["get" + name] = function() { return value; };
+    // THe setter method stores the value or throws an exception if
+    // the predicate rejects the value.
+    o["set" + name] = function(v) {
+        if(predicate && !predicate(v))
+            throw Error("set" + name + ": invalid value " + v);
+        else
+            value = v;
+    };
+}
+
+// Replace the method named m of the object o with a version that logs 
+// messages before and after invoking the original method.
+function trace(o, m) {
+    var original = o[m];    // Remember the original method in the closure
+    o[m] = function() {     // Now define the new method
+        console.log(new Date(), "Entering:", m);        // Log message.
+        var result = original.apply(this, arguments);   // Invoke original
+        console.log(new Date(), "Exiting:", m);         // Log message
+        return result;
+    }
+}
+
+// Return a function that invokes f as a method of o, passing all the arguments
+function bind(f, o) {
+    if(f.bind) return f.bind(o);    // Use the bind method, if ther is one
+    else return function() {        // Otherwise, bind it like this
+        return f.apply(o, arguments);
+    };
+}
+
+// Function to perform partial application and binding for ECMAScript 3
+// Has issues that it does not have a length property and cannot be used for constructor functions
+if(!Function.prototype.bind) {
+    Function.prototype.bind = function(o, /*, args */) {
+        // Save the this and arguments values into variables so we
+        // can use them in the nested functions below.
+        var self = this, boundArgs = arguments;
+
+        // The return value of the bind() method is a function
+        return function() {
+            // Build up an argument list, starting with any args passed
+            // to bind after the first one, and follow those with all args
+            // passed to this function
+            var args = [], i;
+            for(i = 1; i < boundArgs.length; i++) args.push(boundArgs[i]);
+            for(i = 0; i < arguments.length; i++) args.push(arguments[i]);
+
+                // Now invoke self as a method of o, with those arguments
+            return self.apply(o, args);
+        }
+    }
+}
+
+// Call the function f for each element of array a and return
+// an array of results. Use Array.prototype.map if it is defined
+var map = Array.prototype.map
+    ? function(a, f) { return a.map(f); }   // Ue map method if it exists
+    : function(a,f) {                       // Otherwise, implement our own
+        var results = [];
+        for (var i = 0, len = a.length; i < len; i++) {
+            if(i in a) results[i] = f.call(null, a[i], i, a);
+        };
+        return results;
+    };
+
+// Reduce the array to a single value using the function f and 
+// optional initial value. Use Array.prototype.reduce if it is defined.
+var reduce = Array.prototype.reduce
+    ? function(a, f, initial)   {   // If the reduce() method exists
+        if(arguments.length > 2)
+            return a.reduce(f, initial);    // If an initial value was passed
+        else
+            return a.reduce(f);             // Otherwise, no initial value.
+    } 
+    : function(a, f, initial) {             // This algorithm from the ES5 specification
+        var i = 0, len = a.length, accumulator;
+        // Start with the specified initial value, or the first value in a
+        if(arguments.length > 2) accumulator = initial;  
+        else {  // Find the first defined index in the array
+            if(len == 0)  throw TypeError();
+            while(i < len) {
+                if(i in a) {
+                    accumulator = a[i++];
+                    break;
+                }
+                else i++;
+            }
+            if(i == len) throw TypeError();
+        }
+
+        // Now call f for each remaining element in the array
+        while(i < len) {
+            if(i in a)
+                accumulator = f.call(undefined, accumulator, a[i], i, a);
+            i++;
+        }
+        return accumulator;
+    };
+
+
+
+
+
+       
+
